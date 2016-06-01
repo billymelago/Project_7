f0a150c1636da8d717073c09b3e1852f68845252
Billy Melago <billymelago@gmail.com>
2016-05-31 20:16:52 -0400

    interactive video player

---
+++ video_player_v2/video_player_v2/js/app.js
@@ -0,0 +1,338 @@
+/*document.addEventListener("DOMContentLoaded", function() {
+    var track = document.getElementById("track-1");
+    track.addEventListener("cuechange", function() {
+        var myTrack = this.track;
+        var myCues = myTrack.activeCues;
+        if(myCues.length > 0) {
+            var txtWrapper = document.getElementById("txt-wrapper");
+            txtWrapper.replaceChild((myCues[0].getCueAsHTML()), txtWrapper.firstChild);
+        }
+    },false);
+}, false);
+
+document.getElementById("myBtn").addEventListener("click", function () {
+    var myTrack = document.getElementById("track-1").track;
+    var myCues = myTrack.cues;
+    for (var i = 0; i < myCues.length; i++) {
+        document.getElementById("txt-wrapper").innerHTML += (myCues[i].getCueAsHTML().textContent + "<br/>");
+    }
+}, false);
+      function getCues() {          
+        var myTrack = document.getElementById("track-1").track; // get text track from track element          
+        var myCues = myTrack.cues;   // get list of cues                    
+        for (var i = 0; i < myCues.length; i++) {
+               //document.getElementById("display").innerHTML += (myCues[i].getCueAsHTML().textContent + "<br/>");  //append track label
+            document.getElementById("display").innerHTML += (myCues[i].text + "<br/>");  //append track label
+        }
+      }*/
+
+   
+    //Variables foe each button
+var videoContainer = document.getElementById('videoContainer');
+var videoOverlay = document.getElementById('overlay');
+var videoControls = document.getElementById('video-controls');
+var playpause = document.getElementById('playpause');
+var replay = document.getElementById('replay');
+var video = document.getElementById('video');
+var mute = document.getElementById('mute');
+var vidFwdThirtySec = document.getElementById('vidFwdThirtySec');
+var volumeslider = document.getElementById('volumeslider');
+var progress = document.getElementById('progress');
+var progressBar = document.getElementById('progress-bar');
+var videoTime = document.getElementById('video-time');
+var currentTimeText = document.getElementById('curTimeText');
+var durationTimeText = document.getElementById('durTimeText');
+var otherButtons = document.getElementById('otherBtns');
+var fullscreen = document.getElementById('fs');
+   
+
+window.addEventListener("keydown", checkKeyPressed, false);
+function checkKeyPressed(e) {
+    switch (e.keyCode) {
+    case 37://Left arrow key
+        video.currentTime -= 2;
+        break;
+    case 39://Up arrow
+        video.currentTime += 2;
+        break;
+    case 107://Add (+) key
+        volumeslider.value = 100;
+        video.volume = 100;
+        break;
+    case 109://Subtract (-) key
+        volumeslider.value = 0;
+        video.volume = 0;
+        break;
+    }
+}
+
+//IIF
+(function () {
+    //Turn off default controls
+    video.controls = false;
+    $("#video").bind("ended", function () {
+        video.currentTime = 0;
+        progress.value = 0;
+        video.load();
+        $("#otherBtns").css("display", "flex");
+        $("#playpause").css("background-image", "url('../icons/play-icon.png')");
+    });
+    $("#video-transcript").accordion({
+        collapsible: true,
+        active: false,
+        heightStyle: "content"
+    });
+   
+    //Turn on custom cotrols
+    videoControls.setAttribute('data-state', 'visible');
+    
+    var subtitles = document.getElementById('subtitles');
+    for (var i = 0; i < video.textTracks.length; i++) {
+        video.textTracks[i].mode = 'hidden';
+    }
+   
+    // Creates and returns a menu item for the subtitles language menu
+    var subtitlesMenuButtons = [];
+    var createMenuItem = function(id, lang, label) {
+        var listItem = document.createElement('li');
+        var button = listItem.appendChild(document.createElement('button'));
+        button.setAttribute('id', id);
+        button.className = 'subtitles-button';
+        if (lang.length > 0) button.setAttribute('lang', lang);
+        button.value = label;
+        button.setAttribute('data-state', 'inactive');
+        button.appendChild(document.createTextNode(label));
+        button.addEventListener('click', function(e) {
+            //Setting all the buttons to inactive
+            subtitlesMenuButtons.map(function(v, i, a) {
+                subtitlesMenuButtons[i].setAttribute('data-state', 'inactive');
+        });
+        //Find the language to activate
+        var lang = this.getAttribute('lang');
+        for (var i = 0; i < video.textTracks.length; i++) {
+            //For the subtitles-off button the first condition will never match so all will subtitles be turned off
+            if (video.textTracks[i].language == lang) {
+                video.textTracks[i].mode = 'showing';
+                this.setAttribute('data-state', 'active');
+            }
+            else {
+                video.textTracks[i].mode = 'hidden';
+            }
+        }
+        subtitlesMenu.style.display = 'none';
+    });
+    subtitlesMenuButtons.push(button);
+    return listItem;    
+    }
+     //Building a caption menu
+    var subtitlesMenu;
+    if (video.textTracks) {
+       var df = document.createDocumentFragment();
+       var subtitlesMenu = df.appendChild(document.createElement('ul'));
+       subtitlesMenu.className = 'subtitles-menu';
+       subtitlesMenu.appendChild(createMenuItem('subtitles-off', '', 'Off'));
+       for (var i = 0; i < video.textTracks.length; i++) {
+          subtitlesMenu.appendChild(createMenuItem('subtitles-' + video.textTracks[i].language, video.textTracks[i].language, video.textTracks[i].label));
+       }
+       videoContainer.appendChild(subtitlesMenu);
+    }
+    
+    subtitles.addEventListener('click', function(e) {
+        if (subtitlesMenu) {
+            subtitlesMenu.style.display = (subtitlesMenu.style.display == 'block' ? 'none' : 'block');
+        }
+    });
+})();
+
+//Hide video controls on mouse out
+var hoverFunction = function() {
+    videoContainer.onmouseout = function(){
+        otherButtons.style.display = 'flex';
+        otherButtons.style.opacity = '0';
+        videoTime.style.opacity = '0';
+        videoOverlay.style.opacity = '0';
+    };
+    videoContainer.onmouseover = function(){
+        otherButtons.style.display = 'flex';
+        otherButtons.style.opacity = '1';
+        videoTime.style.opacity = '1';
+        videoOverlay.style.opacity = '.45';
+    };
+};
+
+
+//Functions for basic buttons
+var vidplay = function () {
+        if (video.paused) {
+            video.play();
+            playpause.setAttribute('data-state', 'play');
+            
+            videoControls.onmouseout = hoverFunction;
+            videoContainer.onmouseout = hoverFunction;
+        } else {
+            video.pause();
+            playpause.setAttribute('data-state', 'pause');
+            videoControls.onmouseout = function(){
+            otherButtons.style.display = 'flex'; 
+    };
+            videoContainer.onmouseout = function(){
+            otherButtons.style.display = 'flex';    
+    };
+        } 
+    }
+var replayVideo = function () {
+            video.pause();
+			video.currenttime = 0;
+			video.load();
+            playpause.setAttribute('data-state', 'play');
+            video.play();
+}
+
+var muteSound = function() {
+    video.muted = !video.muted;
+    if(video.muted) {
+       volumeslider.value = 0;
+        mute.setAttribute('data-state', 'mute');
+    } if(!video.muted) {
+       volumeslider.value = 100;
+        mute.setAttribute('data-state', 'unmute');
+    }
+}
+
+var setVolume = function() {
+    var seekto = volumeslider.value / 100;
+    video.volume = seekto;
+};
+var fwdThirtySec = function() {
+  video.currentTime += 10;  
+};
+var seekTimeUpdate = function() {
+    var newTime = video.currentTime * (100 / video.duration);
+    seekslider.value = newTime;
+};
+var progressFunction = function() {
+    progress.setAttribute('max', video.duration);
+};
+var progressBarFunction = function() {
+    progress.value = video.currentTime;
+    progressBar.style.width = Math.floor((video.currentTime / video.duration) * 100);
+    var curmins = Math.floor(video.currentTime / 60);
+    var cursecs = Math.floor(video.currentTime - curmins * 60);
+    var durmins = Math.floor(video.duration / 60);
+    var dursecs = Math.floor(video.duration - durmins * 60);
+    if(cursecs <= 10) {
+        cursecs = "0"+cursecs;
+    }
+    if(dursecs <= 10) {
+        dursecs = "0"+dursecs;
+    }
+    currentTimeText.innerHTML = curmins+":"+cursecs;
+    durationTimeText.innerHTML = durmins+":"+dursecs;
+};
+var progressBarVidSeek = function(e) {
+    var pos = (e.pageX - this.offsetLeft) / this.offsetWidth;
+    video.currentTime = pos * video.duration;
+};
+var ccFunction = function() {
+    var track = document.getElementById("track-1");
+    track.addEventListener("cuechange", function() {
+        var myTrack = this.track;
+        var myCues = myTrack.activeCues;
+        if(myCues.length > 0) {
+            var txtWrapper = document.getElementById("display");
+            txtWrapper.replaceChild((myCues[0].getCueAsHTML()), txtWrapper.firstChild);
+        }
+    },false);
+    console.log('its working');
+};
+var myFullscreenFunction = function() {
+    var elem = document.getElementById("video");
+        if (elem.requestFullscreen) {
+          elem.requestFullscreen();
+        } else if (elem.msRequestFullscreen) {
+          elem.msRequestFullscreen();
+        } else if (elem.mozRequestFullScreen) {
+          elem.mozRequestFullScreen();
+        } else if (elem.webkitRequestFullscreen) {
+          elem.webkitRequestFullscreen();
+        }
+    };
+function toggleFullScreen() {
+  if (!document.fullscreenElement &&    // alternative standard method
+      !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {  // current working methods
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) {
+      document.documentElement.msRequestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+      document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+}
+//Another user defined function — setFullscreenData() — is also called, which simply sets the value of a data-fullscreen attribute on the videoContainer (this makes use of data-states).
+var setFullscreenData = function(state) {
+   videoContainer.setAttribute('data-fullscreen', !!state);
+}
+//This is used simply to set some basic CSS to improve the styling of the custom controls when they are in fullscreen (see the sample code for further details). When a video goes into fullscreen mode, it usually displays a message indicating that the user can press the Esc key to exit fullscreen mode, so the code also needs to listen for relevant events in order to call the setFullscreenData() function to ensure the control styling is correct:
+document.addEventListener('fullscreenchange', function(e) {
+   setFullscreenData(!!(document.fullScreen || document.fullscreenElement));
+});
+document.addEventListener('webkitfullscreenchange', function() {
+   setFullscreenData(!!document.webkitIsFullScreen);
+});
+document.addEventListener('mozfullscreenchange', function() {
+   setFullscreenData(!!document.mozFullScreen);
+});
+document.addEventListener('msfullscreenchange', function() {
+   setFullscreenData(!!document.msFullscreenElement);
+});
+
+
+
+//These happen when the user clicks on one off the custom media buttons
+
+playpause.onclick = vidplay;
+replay.onclick = replayVideo;
+mute.onclick = muteSound;
+volumeslider.onclick = setVolume;
+video.ontimeupdate = seekTimeUpdate;
+video.onloadedmetadata = progressFunction;
+video.ontimeupdate = progressBarFunction;
+progress.onclick = progressBarVidSeek;
+vidFwdThirtySec.onclick = fwdThirtySec;
+fullscreen.onclick = myFullscreenFunction;
+videoOverlay.onclick = vidplay;
+//videoContainer.onmouseout = hoverFunction;
+
+//This adds fullscreen mode if user clicks 'enter'
+document.addEventListener("keydown", function(e) {
+  if (e.keyCode == 13) {
+    toggleFullScreen();
+  }
+}, false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ video_player_v2/video_player_v2/js/transcript.js
@@ -0,0 +1,221 @@
+
+var videoPlayerUI = {
+    // Defining transcript elements
+		transcriptContainer: $('.transcript'),
+		ph01: $('.ph01'),
+		ph02: $('.ph02'),
+		ph03: $('.ph03'),
+		ph04: $('.ph04'),
+		ph05: $('.ph05'),
+		ph06: $('.ph06'),
+		ph07: $('.ph07'),
+		ph08: $('.ph08'),
+		ph09: $('.ph09'),
+		ph10: $('.ph10'),
+		ph11: $('.ph11'),
+		ph12: $('.ph12'),
+		ph13: $('.ph13'),
+		ph14: $('.ph14'),
+		ph15: $('.ph15'),
+		ph16: $('.ph16'),
+
+// Function to hilight current phrase on timeupdate event
+        transcriptHighlight: function () {
+            var myVideo = document.getElementById('video');
+            $(myVideo).on('timeupdate', function () {
+				var currTime = myVideo.currentTime;
+				if (currTime > 0 && currTime < 4 ) {
+				    videoPlayerUI.ph01.css('color', 'white');
+                    videoPlayerUI.ph01.css('font-size', '1.25em');
+				} else {
+				    videoPlayerUI.ph01.css('color', 'mediumpurple');
+                    videoPlayerUI.ph01.css('font-size', '1em');
+				}
+				if (currTime > 4 && currTime < 7.5 ) {
+				    videoPlayerUI.ph02.css('color', 'white');
+                    videoPlayerUI.ph02.css('font-size', '1.25em');
+				} else {
+				    videoPlayerUI.ph02.css('color', 'mediumpurple');
+                    videoPlayerUI.ph02.css('font-size', '1em');
+				}
+				if (currTime > 7.5 && currTime < 11.2 ) {
+				    videoPlayerUI.ph03.css('color', 'white');
+                    videoPlayerUI.ph03.css('font-size', '1.25em');
+				} else {
+				    videoPlayerUI.ph03.css('color', 'mediumpurple');
+                    videoPlayerUI.ph03.css('font-size', '1em');
+				}
+				if (currTime > 11.2 && currTime < 14 ) {
+				    videoPlayerUI.ph04.css('color', 'white');
+                    videoPlayerUI.ph04.css('font-size', '1.25em');
+				} else {
+				    videoPlayerUI.ph04.css('color', 'mediumpurple');
+                    videoPlayerUI.ph04.css('font-size', '1em');
+				}
+				if (currTime > 14 && currTime < 18 ) {
+				    videoPlayerUI.ph05.css('color', 'white');
+                    videoPlayerUI.ph05.css('font-size', '1.25em');
+				} else {
+				    videoPlayerUI.ph05.css('color', 'mediumpurple');
+                    videoPlayerUI.ph05.css('font-size', '1em');
+				}
+				if (currTime > 18 && currTime < 22.4 ) {
+				    videoPlayerUI.ph06.css('color', 'white');
+                    videoPlayerUI.ph06.css('font-size', '1.25em');
+				} else {
+				    videoPlayerUI.ph06.css('color', 'mediumpurple');
+                    videoPlayerUI.ph06.css('font-size', '1em');
+				}
+				if (currTime > 22.4 && currTime < 26.9 ) {
+				    videoPlayerUI.ph07.css('color', 'white');
+                    videoPlayerUI.ph07.css('font-size', '1.25em');
+				} else {
+				    videoPlayerUI.ph07.css('color', 'mediumpurple');
+                    videoPlayerUI.ph07.css('font-size', '1em');
+                }
+                if (currTime > 26.9 && currTime < 31 ) {
+                    videoPlayerUI.ph08.css('color', 'white');
+                    videoPlayerUI.ph08.css('font-size', '1.25em');
+                } else {
+                    videoPlayerUI.ph08.css('color', 'mediumpurple');
+                    videoPlayerUI.ph08.css('font-size', '1em');
+                }
+                if (currTime > 32.1 && currTime < 34.7 ) {
+                    videoPlayerUI.ph09.css('color', 'white');
+                    videoPlayerUI.ph09.css('font-size', '1.25em');
+                } else {
+                    videoPlayerUI.ph09.css('color', 'mediumpurple');
+                    videoPlayerUI.ph09.css('font-size', '1em');
+                }
+                if (currTime > 34.7 && currTime < 39.5 ) {
+                    videoPlayerUI.ph10.css('color', 'white');
+                    videoPlayerUI.ph10.css('font-size', '1.25em');
+                } else {
+                    videoPlayerUI.ph10.css('color', 'mediumpurple');
+                    videoPlayerUI.ph10.css('font-size', '1em');
+                }
+                if (currTime > 39.5 && currTime < 41.2 ) {
+                    videoPlayerUI.ph11.css('color', 'white');
+                    videoPlayerUI.ph11.css('font-size', '1.25em');
+                } else {
+                    videoPlayerUI.ph11.css('color', 'mediumpurple');
+                    videoPlayerUI.ph11.css('font-size', '1em');
+                }
+                if (currTime > 42.3 && currTime < 46.3 ) {
+                    videoPlayerUI.ph12.css('color', 'white');
+                    videoPlayerUI.ph12.css('font-size', '1.25em');
+                } else {
+                    videoPlayerUI.ph12.css('color', 'mediumpurple');
+                    videoPlayerUI.ph12.css('font-size', '1em');
+                }
+                if (currTime > 46.3 && currTime < 49.3 ) {
+                    videoPlayerUI.ph13.css('color', 'white');
+                    videoPlayerUI.ph13.css('font-size', '1.25em');
+                } else {
+                    videoPlayerUI.ph13.css('color', 'mediumpurple');
+                    videoPlayerUI.ph13.css('font-size', '1em');
+                }
+                if (currTime > 49.3 && currTime < 53.7 ) {
+                    videoPlayerUI.ph14.css('color', 'white');
+                    videoPlayerUI.ph14.css('font-size', '1.25em');
+                } else {
+                    videoPlayerUI.ph14.css('color', 'mediumpurple');
+                    videoPlayerUI.ph14.css('font-size', '1em');
+                }
+                if (currTime > 53.8 && currTime < 57.8 ) {
+                    videoPlayerUI.ph15.css('color', 'white');
+                    videoPlayerUI.ph15.css('font-size', '1.25em');
+                } else {
+                    videoPlayerUI.ph15.css('color', 'mediumpurple');
+                    videoPlayerUI.ph15.css('font-size', '1em');
+                }
+                if (currTime > 57.8 && currTime < 59 ) {
+                    videoPlayerUI.ph16.css('color', 'white');
+                    videoPlayerUI.ph16.css('font-size', '1.25em');
+                } else {
+                    videoPlayerUI.ph16.css('color', 'mediumpurple');
+                    videoPlayerUI.ph16.css('font-size', '1em');
+                };
+            });
+				
+},
+
+			// Function to allow clicking on a phrase to set video current time respectively
+        transcriptClick: function() {
+             var myVideo = document.getElementById('video');
+            videoPlayerUI.ph01.click(function() {
+                myVideo.currentTime = 0;
+                myVideo.play();
+            });
+            videoPlayerUI.ph02.click(function() {
+                myVideo.currentTime = 4;
+                myVideo.play();
+            });
+            videoPlayerUI.ph03.click(function() {
+                myVideo.currentTime = 7.5;
+                myVideo.play();
+            });
+            videoPlayerUI.ph04.click(function() {
+                myVideo.currentTime = 11.2;
+                myVideo.play();
+            });
+            videoPlayerUI.ph05.click(function() {
+                myVideo.currentTime = 14;
+                myVideo.play();
+            });
+            videoPlayerUI.ph06.click(function() {
+                myVideo.currentTime = 18;
+                myVideo.play();
+            });
+            videoPlayerUI.ph07.click(function() {
+                myVideo.currentTime = 22.4;
+                myVideo.play();
+            });
+            videoPlayerUI.ph08.click(function() {
+                myVideo.currentTime = 26.9;
+                myVideo.play();
+            });
+            videoPlayerUI.ph09.click(function() {
+                myVideo.currentTime = 32.1;
+                myVideo.play();
+            });
+            videoPlayerUI.ph10.click(function() {
+                myVideo.currentTime = 34.7;
+                myVideo.play();
+            });
+            videoPlayerUI.ph11.click(function() {
+                myVideo.currentTime = 39.5;
+                myVideo.play();
+            });
+            videoPlayerUI.ph12.click(function() {
+                myVideo.currentTime = 42.3;
+                myVideo.play();
+            });
+            videoPlayerUI.ph13.click(function() {
+                myVideo.currentTime = 46.3;
+                myVideo.play();
+            });
+            videoPlayerUI.ph14.click(function() {
+                myVideo.currentTime = 49.3;
+                myVideo.play();
+            });
+            videoPlayerUI.ph15.click(function() {
+                myVideo.currentTime = 53.8;
+                myVideo.play();
+            });
+            videoPlayerUI.ph16.click(function() {
+                myVideo.currentTime = 57.8;
+                myVideo.play();
+            });
+        },
+                    // Function to load controls
+        loadControls: function() {
+                videoPlayerUI.transcriptHighlight();
+                videoPlayerUI.transcriptClick();
+        }
+};
+
+setInterval(function(t) {
+	videoPlayerUI.loadControls();
+}, 1000);
+
